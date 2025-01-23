4a86ab6ee3c11b1ff291747f8e5a71c3680dfd4c
GunduSriBhanu <sribhanu5june@gmail.com>
2025-01-23 14:54:48 -0800

    Modified CDK

--- multi-agents-cdk/multi_agents_cdk/multi_agents_cdk_stack.py
+++ multi-agents-cdk/multi_agents_cdk/multi_agents_cdk_stack.py
@@ -4,6 +4,9 @@ from aws_cdk import (
     aws_lambda as _lambda,
     aws_ecr_assets as ecr_assets,
     # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    CfnOutput,
+    CfnTag,
 )
 from constructs import Construct
 import os 
@@ -12,28 +15,72 @@ class MultiAgentsCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-        docker_directory = "./docker"
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MultiAgentsCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
-
-        if not os.path.exists(docker_directory):
-            raise FileNotFoundError(f"The specified directory {docker_directory} does not exist.")
-        
-        # Build Docker image
-        asset = ecr_assets.DockerImageAsset(
-            self, "Python312Image",
-            directory=docker_directory,  # Must be a static, resolvable path
+        # Create Basic VPC
+        vpc = ec2.Vpc(
+            self,
+            "MyVpc",
+            max_azs=2,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="public-subnet-1",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24,
+                )
+            ],
         )
 
+        # Create Security Group
+        sec_group = ec2.SecurityGroup(
+            self,
+            "MySecurityGroup",
+            vpc=vpc,
+            allow_all_outbound=True,  # Allow all outbound traffic
+        )
 
-        # Create Lambda function using the container image
-        _lambda.Function(
-            self, "Python312Function",
-            code=_lambda.Code.from_asset_image(asset.image_uri), 
-            handler=_lambda.Handler.FROM_IMAGE,
-            runtime=_lambda.Runtime.FROM_IMAGE,
+        # Add Ingress Rule for SSH (Port 22)
+        sec_group.add_ingress_rule(
+            ec2.Peer.any_ipv4(),  # Allow traffic from any IPv4 address
+            ec2.Port.tcp(22),     # Port 22 for SSH
+            "Allow SSH access"
+        )
+
+        # Add Ingress Rule for Port 8000 (Custom Application)
+        sec_group.add_ingress_rule(
+            ec2.Peer.any_ipv4(),  # Allow traffic from any IPv4 address
+            ec2.Port.tcp(8000),   # Port 8000
+            "Allow access to port 8000 from anywhere"
+        )
+
+        # Create Key Pair
+        cfn_key_pair = ec2.CfnKeyPair(
+            self,
+            "MyCfnKeyPair",
+            key_name="cdk-ec2-key-pair-med",
+            tags=[CfnTag(key="key", value="value")],
+        )
+
+        # Create EC2 Instance with Amazon Linux 2023 AMI
+        instance = ec2.Instance(
+            self,
+            "MyInstance",
+            instance_type=ec2.InstanceType("t2.medium"),
+            machine_image=ec2.MachineImage.latest_amazon_linux2023(),
+            vpc=vpc,
+            security_group=sec_group,
+            associate_public_ip_address=True,
+            key_name=cfn_key_pair.key_name,
+        )
+
+        # Output the Instance ID
+        CfnOutput(
+            self, "InstanceId",
+            value=instance.instance_id,
+            description="Instance ID of the created EC2 instance",
+        )
+
+        _lambda.DockerImageFunction(
+            self,
+            "DockerLambda",
+            code=_lambda.DockerImageCode.from_image_asset(r"multi_agents_cdk\lambda\python"),
+            function_name="DockerLambdaFunction"
         )
